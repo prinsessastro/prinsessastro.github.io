527f84be8c231ce025b708d7c5827cb5887d3524
Määttä Susanna Maria <susanna.maatta@edu.tampere.fi>
2020-10-29 13:36:41 +0200

    build

---
+++ build/precache-manifest.dac23cb5d878abf8e900ff0af569856b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88fc52ccd3a1b67116c7a499f09dab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e404fe277c5a6d43fcf7",
+    "url": "/static/css/main.24c0a2a3.chunk.css"
+  },
+  {
+    "revision": "6490eb9e60a46680ac5d",
+    "url": "/static/js/2.3537dd4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3537dd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e404fe277c5a6d43fcf7",
+    "url": "/static/js/main.e7ea04eb.chunk.js"
+  },
+  {
+    "revision": "5cae5f6954cc5f5d3f4c",
+    "url": "/static/js/runtime-main.568042db.js"
+  },
+  {
+    "revision": "bcda2725e2bbb573d6135292f1501701",
+    "url": "/static/media/forshop.bcda2725.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
