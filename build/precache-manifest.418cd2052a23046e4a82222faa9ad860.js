beb589a0b2fa3d4069857ed3ece210b301312da7
Määttä Susanna Maria <susanna.maatta@edu.tampere.fi>
2020-10-29 13:47:42 +0200

    buildikorjattu

---
+++ build/precache-manifest.418cd2052a23046e4a82222faa9ad860.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f7b1a9ba6274c3cb9790ab89bdbc7e",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "92910484b51f42a5f5c6",
+    "url": "/build/static/css/main.24c0a2a3.chunk.css"
+  },
+  {
+    "revision": "6490eb9e60a46680ac5d",
+    "url": "/build/static/js/2.3537dd4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/build/static/js/2.3537dd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92910484b51f42a5f5c6",
+    "url": "/build/static/js/main.24e152c7.chunk.js"
+  },
+  {
+    "revision": "8094f22b29b4b70dc316",
+    "url": "/build/static/js/runtime-main.b980811f.js"
+  },
+  {
+    "revision": "bcda2725e2bbb573d6135292f1501701",
+    "url": "/build/static/media/forshop.bcda2725.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/build/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dac23cb5d878abf8e900ff0af569856b.js"
+  "/build/precache-manifest.418cd2052a23046e4a82222faa9ad860.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
